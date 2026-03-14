b3fe08e770093c2340ba09483bc1b6d61dff7ff7
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2026-03-14 11:31:31 +0200

    new modes
     work in progress

--- js/cards.js
+++ js/cards.js
@@ -1,135 +1,170 @@
-selected = {"question":0,"answer":0};
-previousSelection = '';
+selected = { question: 0, answer: 0 };
+previousSelection = "";
 let score = 0;
 let wrong = 0;
 // function to jumble lists for matching activities
-const randomise = (x)=>{
-    let y = []
-    while(x.length>0){
-        y.push(String(x.splice(Math.floor(Math.random()*x.length-1),1)));
-    }
-    return y
+const randomise = (x) => {
+  let y = [];
+  while (x.length > 0) {
+    y.push(String(x.splice(Math.floor(Math.random() * x.length - 1), 1)));
+  }
+  return y;
 };
+// set up basic flashcards roll
+const buildFlashCards = (target, data) => {
+  console.log("chose flashes");
+};
+
 //function that build matching activities
-const buildMatchem= (target,data)=>{
+const buildMatchem = (target, data) => {
+  let qs = data;
 
-    let qs = data;
+  console.log(qs);
 
-    console.log(qs)
+  let questions = Object.keys(qs);
+  let answers = Object.values(qs);
 
-    let questions = Object.keys(qs);
-    let answers = Object.values(qs);
+  let randans = randomise(answers);
 
-    let randans = randomise(answers);
+  for (let i = 0; i < questions.length; i++) {
+    let question = questions[i];
+    let answer = randans[i];
 
-    for(let i = 0; i<questions.length;i++){
-        let question = questions[i];
-        let answer = randans[i];
-    
-        console.log(question,answer)
-        
-        document.getElementById(target).innerHTML+=`<div class = "matchem_row">
+    console.log(question, answer);
+
+    document.getElementById(target).innerHTML += `<div class = "matchem_row">
         <div class = "selectable question " data-type="">${question}</div>
         <div class = "selectable answer " data-type="">${answer}</div>
         </div>`;
-    }
-    activateListeners();
-    score = 0;
-    return 1
+  }
+  activateListeners();
+  score = 0;
+  return 1;
 };
+
+const one_to_four = (target, data, dir) => {
+  console.log("chose 124");
+};
+
+const typingTask = (target, data, dir) => {
+  console.log("chose type");
+};
+
+function buildTask(e) {
+  actions = {
+    bb: () => hideShow("task_menu"),
+    flashcard: () => buildFlashCards("show_zone", flashCards[`${selectedSet}`]),
+    matchems: () => buildMatchem("show_zone", flashCards[`${selectedSet}`]),
+    onetofour: () =>
+      buildOneToFour("show_zone", flashCards[`${selectedSet}`], 1),
+    onetofour2: () =>
+      buildOneToFour("show_zone", flashCards[`${selectedSet}`], 0),
+    type: () => buildOneToFour("show_zone", flashCards[`${selectedSet}`], 0),
+    type2: () => buildOneToFour("show_zone", flashCards[`${selectedSet}`], 0),
+  };
+
+  actions[e.target.id]();
+  console.log("picked", e.target.id);
+  hideShow("task_menu");
+}
+
 // use two elements to check if they match
-const checkMatch = (newSelection,selected)=>{
-    console.log('adding picked')
+const checkMatch = (newSelection, selected) => {
+  console.log("adding picked");
 
-    // add the picked class to the picked element
-    newSelection.classList.add("picked");
-    
-    // clear out selected to ensure wrong is wrong!
-    selected["question"]!=0 & newSelection.classList.contains("question")? selected["answer"] = 0:null;
-    selected["answer"]!=0 & newSelection.classList.contains("answer")? selected["question"] = 0:null;
-    console.log(selected);
+  // add the picked class to the picked element
+  newSelection.classList.add("picked");
 
-    // fill in the selected variable
-    newSelection.classList.contains("question")? selected["question"]= newSelection.innerHTML:0;
-    newSelection.classList.contains("answer")? selected["answer"]= newSelection.innerHTML:0;
-    console.log(selected)
-  
-    // set variables for comparison
-    let question = selected["question"];
-    let answer = selected["answer"]
-    let scorePanel = document.getElementById('score');
-    // let type = selected["eword"].dataset.type;
-    // console.log(question,answer)
+  // clear out selected to ensure wrong is wrong!
+  (selected["question"] != 0) & newSelection.classList.contains("question")
+    ? (selected["answer"] = 0)
+    : null;
+  (selected["answer"] != 0) & newSelection.classList.contains("answer")
+    ? (selected["question"] = 0)
+    : null;
+  console.log(selected);
 
-    if(flashCards[selectedSet][`${selected["question"]}`] ==  answer ){
-        
-        console.log("MATCH MATCH MATCH");
+  // fill in the selected variable
+  newSelection.classList.contains("question")
+    ? (selected["question"] = newSelection.innerHTML)
+    : 0;
+  newSelection.classList.contains("answer")
+    ? (selected["answer"] = newSelection.innerHTML)
+    : 0;
+  console.log(selected);
 
-        // change style to correct
-        newSelection.classList.add("correct");
-        previousSelection.classList.add("correct");
-        
-        //reset the selected variable and remove other class
-        newSelection.classList.remove('picked')
-        previousSelection.classList.remove('picked')
-        newSelection = ''
-        previousSelection = ''
-        score+=1;
+  // set variables for comparison
+  let question = selected["question"];
+  let answer = selected["answer"];
+  let scorePanel = document.getElementById("score");
+  // let type = selected["eword"].dataset.type;
+  // console.log(question,answer)
 
+  if (flashCards[selectedSet][`${selected["question"]}`] == answer) {
+    console.log("MATCH MATCH MATCH");
+
+    // change style to correct
+    newSelection.classList.add("correct");
+    previousSelection.classList.add("correct");
+
+    //reset the selected variable and remove other class
+    newSelection.classList.remove("picked");
+    previousSelection.classList.remove("picked");
+    newSelection = "";
+    previousSelection = "";
+    score += 1;
+  } else {
+    console.log(newSelection);
+    console.log(previousSelection);
+
+    // duplicate variables to avoid losing before set Timeout processes
+    const a = newSelection;
+    const b = previousSelection;
+    if ((newSelection != "") & (previousSelection != "")) {
+      wrong += 1;
+
+      console.log("setting timer");
+      setTimeout(function () {
+        console.log(a, b);
+        a.classList.remove("picked");
+        b.classList.remove("picked");
+        newSelection = "";
+        previousSelection = "";
+      }, 500);
+      console.log("remove picked");
     }
-    else{
-        console.log(newSelection);
-        console.log(previousSelection);
 
-        // duplicate variables to avoid losing before set Timeout processes
-        const a = newSelection;
-        const b = previousSelection; 
-        if(newSelection != '' & previousSelection !=''){
-            wrong+=1;
-
-            console.log('setting timer')
-            setTimeout(function (){
-
-            
-            console.log(a,b)
-            a.classList.remove('picked');
-            b.classList.remove('picked');
-            newSelection = '';
-            previousSelection = '';
-            
-            }
-            ,500);
-            console.log('remove picked')
-        }
-    
     previousSelection = newSelection;
-    console.log(selected)
-    }
+    console.log(selected);
+  }
 
-    console.log(score);
-    console.log(wrong)
-    scorePanel.innerHTML = `${score}/${Object.keys(flashCards[selectedSet]).length + wrong}`
-    console.log(selected)
+  console.log(score);
+  console.log(wrong);
+  scorePanel.innerHTML = `${score}/${Object.keys(flashCards[selectedSet]).length + wrong}`;
+  console.log(selected);
 
-    // check all are completed and update scores
-    let corrects = document.getElementsByClassName('correct');
-    let selectables = document.getElementsByClassName('selectable')
-    console.log(corrects.length);
-    console.log(selectables.length)
-    if(corrects.length == selectables.length){
-        console.log('all questions completed!!!!\n\n\n\n\n');
-        scores[selectedSet] = `${score}/${Object.keys(flashCards[selectedSet]).length + wrong}`;
-    }
-    else{}
+  // check all are completed and update scores
+  let corrects = document.getElementsByClassName("correct");
+  let selectables = document.getElementsByClassName("selectable");
+  console.log(corrects.length);
+  console.log(selectables.length);
+  if (corrects.length == selectables.length) {
+    console.log("all questions completed!!!!\n\n\n\n\n");
+    scores[selectedSet] =
+      `${score}/${Object.keys(flashCards[selectedSet]).length + wrong}`;
+  } else {
+  }
 };
-//activate eventListeners for matching 
-const activateListeners = ()=>{
-    console.log('adding listeners')
+//activate eventListeners for matching
+const activateListeners = () => {
+  console.log("adding listeners");
 
-    let selectables = document.getElementById('practice').getElementsByClassName("selectable");
-    for(let i = 0;i<selectables.length;i++){
-        selectables[i].addEventListener("pointerdown",(e)=>{
-        checkMatch(e.target,selected)
-        })
-    }
+  let selectables = document
+    .getElementById("practice")
+    .getElementsByClassName("selectable");
+  for (let i = 0; i < selectables.length; i++) {
+    selectables[i].addEventListener("pointerdown", (e) => {
+      checkMatch(e.target, selected);
+    });
+  }
 };
--- js/editadd.js
+++ js/editadd.js
@@ -1,200 +1,207 @@
-let selectedSet = '';
-let tempData = '';
+let selectedSet = "";
+let tempData = "";
 let removeFlag = 0;
 
 // Function that build shows flashcard sets for editting/preview
-const buildMatchemPreview = (target,data,archive)=>{
-    
-    let qs = data;
-    //ensures blank flash cards don't get shown
-    delete qs[''];
-    // get the question and answer pairs
-    let questions = Object.keys(qs);
-    let answers = Object.values(qs);
+const buildMatchemPreview = (target, data, archive) => {
+  let qs = data;
+  //ensures blank flash cards don't get shown
+  delete qs[""];
+  // get the question and answer pairs
+  let questions = Object.keys(qs);
+  let answers = Object.values(qs);
 
-    //cycle through and render in demo_zone
-    for(let i = 0; i<questions.length;i++){
-        let question = questions[i];
-        let answer = answers[i];
-        // console.log(question,answer);
-        // removed selectable class
-        document.getElementById(target).innerHTML+=`<div class = "matchem_row">
+  //cycle through and render in demo_zone
+  for (let i = 0; i < questions.length; i++) {
+    let question = questions[i];
+    let answer = answers[i];
+    // console.log(question,answer);
+    // removed selectable class
+    document.getElementById(target).innerHTML += `<div class = "matchem_row">
         <div class = "unselectable question " data-type="">${question}</div>
         <div class = "unselectable answer " data-type="">${answer}</div>
         </div>`;
-    }
-    if(archive){
-    document.getElementById(target).innerHTML+= "<div id = 'send_to_archive'> <i class='fas fa-archive'></i> </div>"
-    document.getElementById('send_to_archive').addEventListener("pointerdown",(e)=>{
-        archiveData(e)
-    })}
-    return 1
+  }
+  if (archive) {
+    document.getElementById(target).innerHTML +=
+      "<div id = 'send_to_archive'> <i class='fas fa-archive'></i> </div>";
+    document
+      .getElementById("send_to_archive")
+      .addEventListener("pointerdown", (e) => {
+        archiveData(e);
+      });
+  }
+  return 1;
 };
+
 // discard current new set
-const discardChanges = ()=>{
-// explicit hide and show functions?
-    document.getElementById('set_name').value='';
-    document.getElementById('set_select').value='';
-    document.getElementById('question').value = '';
-    document.getElementById('answer').value = '';
-    clear('demo_zone');
-    delete flashCards[selectedSet];
+const discardChanges = () => {
+  // explicit hide and show functions?
+  document.getElementById("set_name").value = "";
+  document.getElementById("set_select").value = "";
+  document.getElementById("question").value = "";
+  document.getElementById("answer").value = "";
+  clear("demo_zone");
+  delete flashCards[selectedSet];
 };
 // Use to redraw the available sets when things change
-function setChanges(){
-    document.getElementById('set_select').innerHTML = '';
-    // build set selection drop-down options
-    let sets = Object.keys(flashCards);
-    document.getElementById('set_select').innerHTML+='<option value="" disabled selected hidden>Choose a flash card set...</option>'
-    for(i=0;i<sets.length;i++){
-        document.getElementById('set_select').innerHTML+=`<option value="${sets[i]}"> ${sets[i]} (${scores[sets[i]]})</option>`;
-    }
-    document.getElementById('set_select').innerHTML+=`<option >new</option>`;
-};
-// Change GUI based on which quiz set is chosen by user
-function handleChange(e){
-    selectedSet = e.target.value;
-    if(selectedSet!='new'){
-        clear('demo_zone');
-        clear('show_zone');
-        buildMatchemPreview('demo_zone', flashCards[`${e.target.value}`],true);
-        buildMatchem('show_zone', flashCards[`${e.target.value}`]);
-        console.log(`selected set is ${selectedSet}`);
-    }
-// explicit hide and show functions?
-    else{
-        console.log('new flashcard set started')
-        // console.log(selectedSet)
-        clear('demo_zone');
-        clear('show_zone');
-        // practiceButton.innerHTML = 'Use cards';
-        document.getElementById('add_sets').classList.remove('hidden');
-        document.getElementById('practice').classList.add('hidden');
-        hide('set_select');
-        show('set_name');
-        practiceButton.innerHTML= 'Go back';
-        // changeButton();
-        show('discard');
-        selectedSet = '';
-    }
-    
-};
-// Toggle element removal when editting mode is on
-function removeMode(){
-
-    if(removeFlag ==0){
-        removeFlag = 1;
-        console.log('activating remove mode');
-                //change button text 
-        document.getElementById('remove_button').innerHTML = 'Done';
-
-        els = document.getElementById('demo_zone').getElementsByClassName('matchem_row');
-        console.log(els.length)
-        for(let i = 0; i<els.length; i++){
-            els[i].innerHTML += '<button class ="remove"><i class="fas fa-trash-alt"></i></button>';
-        }
-
-        let rems = document.getElementsByClassName('remove');
-        
-        for(let i = 0; i<rems.length;i++){
-            rems[i].addEventListener('pointerdown',(e)=>{
-                console.log(e.target.parentElement)
-                let q = e.target.parentElement.parentElement.getElementsByClassName('question')[0];
-                console.log(q)
-                console.log(q.innerHTML)
-                
-                e.target.parentElement.parentElement.remove()
-
-                console.log(flashCards[selectedSet])
-                delete flashCards[selectedSet][q.innerHTML]
-
-                console.log(flashCards[selectedSet])
-
-            })
-        }
-    }
-    else{
-        //change button text 
-        document.getElementById('remove_button').innerHTML = 'remove mode';
-
-        console.log('deactivating remove mode');
-        removeFlag = 0;
-        let els = document.getElementsByClassName('remove');
-        console.log(els.length);
-        while(els[0]){els[0].remove();
-        }
-    }
-};
-// Sen data to the archive
-archiveData = (e)=>{
-    console.log("ITS gonna ARCHIVE BRO!!!",e.target.parentNode.parentNode);
-    console.log(selectedSet)
-    archive[selectedSet]= flashCards[selectedSet];
-    localStorage.setItem('archive', JSON.stringify(archive));
-    delete flashCards[selectedSet];
-    clear('demo_zone');
-    saveData()
+function setChanges() {
+  document.getElementById("set_select").innerHTML = "";
+  // build set selection drop-down options
+  let sets = Object.keys(flashCards);
+  document.getElementById("set_select").innerHTML +=
+    '<option value="" disabled selected hidden>Choose a flash card set...</option>';
+  for (i = 0; i < sets.length; i++) {
+    document.getElementById("set_select").innerHTML +=
+      `<option value="${sets[i]}"> ${sets[i]} (${scores[sets[i]]})</option>`;
+  }
+  document.getElementById("set_select").innerHTML += `<option >new</option>`;
 }
+// Change GUI based on which quiz set is chosen by user
+function handleChange(e) {
+  selectedSet = e.target.value;
+  if (selectedSet != "new") {
+    clear("demo_zone");
+    clear("show_zone");
+    show("task_menu");
+
+    // buildMatchemPreview("demo_zone", flashCards[`${e.target.value}`], true);
+    // buildMatchem("show_zone", flashCards[`${e.target.value}`]);
+    // console.log(`selected set is ${selectedSet}`);
+  }
+  // explicit hide and show functions?
+  else {
+    console.log("new flashcard set started");
+    // console.log(selectedSet)
+    clear("demo_zone");
+    clear("show_zone");
+    // practiceButton.innerHTML = 'Use cards';
+    document.getElementById("add_sets").classList.remove("hidden");
+    document.getElementById("practice").classList.add("hidden");
+    hide("set_select");
+    show("set_name");
+    practiceButton.innerHTML = "Go back";
+    // changeButton();
+    show("discard");
+    selectedSet = "";
+  }
+}
+// Toggle element removal when editting mode is on
+function removeMode() {
+  if (removeFlag == 0) {
+    removeFlag = 1;
+    console.log("activating remove mode");
+    //change button text
+    document.getElementById("remove_button").innerHTML = "Done";
+
+    els = document
+      .getElementById("demo_zone")
+      .getElementsByClassName("matchem_row");
+    console.log(els.length);
+    for (let i = 0; i < els.length; i++) {
+      els[i].innerHTML +=
+        '<button class ="remove"><i class="fas fa-trash-alt"></i></button>';
+    }
+
+    let rems = document.getElementsByClassName("remove");
+
+    for (let i = 0; i < rems.length; i++) {
+      rems[i].addEventListener("pointerdown", (e) => {
+        console.log(e.target.parentElement);
+        let q =
+          e.target.parentElement.parentElement.getElementsByClassName(
+            "question",
+          )[0];
+        console.log(q);
+        console.log(q.innerHTML);
+
+        e.target.parentElement.parentElement.remove();
+
+        console.log(flashCards[selectedSet]);
+        delete flashCards[selectedSet][q.innerHTML];
+
+        console.log(flashCards[selectedSet]);
+      });
+    }
+  } else {
+    //change button text
+    document.getElementById("remove_button").innerHTML = "remove mode";
+
+    console.log("deactivating remove mode");
+    removeFlag = 0;
+    let els = document.getElementsByClassName("remove");
+    console.log(els.length);
+    while (els[0]) {
+      els[0].remove();
+    }
+  }
+}
+// Sen data to the archive
+archiveData = (e) => {
+  console.log("ITS gonna ARCHIVE BRO!!!", e.target.parentNode.parentNode);
+  console.log(selectedSet);
+  archive[selectedSet] = flashCards[selectedSet];
+  localStorage.setItem("archive", JSON.stringify(archive));
+  delete flashCards[selectedSet];
+  clear("demo_zone");
+  saveData();
+};
 // Show elements in the archive and set up restore options
-showArchive = ()=>{
-    let keys=Object.keys(archive);
-    console.log(keys);
-    let addstr = ''
-    for(let i =0;i<keys.length;i++){
-        addstr+=`<div class = 'archived' data-source = '${keys[i]}'>
+showArchive = () => {
+  let keys = Object.keys(archive);
+  console.log(keys);
+  let addstr = "";
+  for (let i = 0; i < keys.length; i++) {
+    addstr += `<div class = 'archived' data-source = '${keys[i]}'>
         <div><p>name: ${keys[i]}</p>
         <p>number of cards: ${Object.keys(archive[keys[i]]).length}</p>
         <p><span>previous score:${scores[keys[i]]}</span></p></div>
         <div><i class="fas fa-trash-restore"></i></div></div>
-        `
-    }
-    
-    return addstr
+        `;
+  }
+
+  return addstr;
 };
 
 // add listeners
 let selecter = document.getElementById("set_select");
 let discard = document.getElementById("discard");
-selecter.addEventListener('change',handleChange)
-discard.addEventListener('pointerdown',discardChanges)
+selecter.addEventListener("change", handleChange);
+discard.addEventListener("pointerdown", discardChanges);
 //listener to add the flash card to the gui and data object
-addButton.addEventListener('pointerdown',()=>{
-    if(selectedSet){
-        console.log(selectedSet)
-        // console.log(flashCards[selectedSet]);
-        let question = document.getElementById('question').value;
-        let answer = document.getElementById('answer').value;
-        //handle unnamed set fix me 07/07/24
-        let set = flashCards[selectedSet];
-        set[question]= answer;
-        console.log(set);
-        clear('demo_zone');
-        buildMatchemPreview('demo_zone',set,true);
-        clear('show_zone')
-        buildMatchem('show_zone',set);
-    }
+addButton.addEventListener("pointerdown", () => {
+  if (selectedSet) {
+    console.log(selectedSet);
+    // console.log(flashCards[selectedSet]);
+    let question = document.getElementById("question").value;
+    let answer = document.getElementById("answer").value;
+    //handle unnamed set fix me 07/07/24
+    let set = flashCards[selectedSet];
+    set[question] = answer;
+    console.log(set);
+    clear("demo_zone");
+    buildMatchemPreview("demo_zone", set, true);
+    clear("show_zone");
+    buildMatchem("show_zone", set);
+  } else if (document.getElementById("set_name").value) {
+    selectedSet = document.getElementById("set_name").value;
 
-    else if(document.getElementById('set_name').value){
-        selectedSet = document.getElementById('set_name').value;
-        
-        let question = document.getElementById('question').value;
-        let answer = document.getElementById('answer').value;
+    let question = document.getElementById("question").value;
+    let answer = document.getElementById("answer").value;
 
-        flashCards[selectedSet]={};
-        flashCards[selectedSet][question] = answer;
-        clear('demo_zone');
+    flashCards[selectedSet] = {};
+    flashCards[selectedSet][question] = answer;
+    clear("demo_zone");
 
-        buildMatchemPreview('demo_zone',flashCards[selectedSet],false);
-    }
-
-    
-    else{
-        console.log(selectedSet)
-        document.getElementById('set_name').value? document.getElementById('set_name').value : window.alert('name your set to make changes');
-    }
+    buildMatchemPreview("demo_zone", flashCards[selectedSet], false);
+  } else {
+    console.log(selectedSet);
+    document.getElementById("set_name").value
+      ? document.getElementById("set_name").value
+      : window.alert("name your set to make changes");
+  }
 });
 setChanges();
-
 
 // Build set selection drop-down options
 // let sets = Object.keys(flashCards);
@@ -202,14 +209,4 @@ setChanges();
 // for(i=0;i<sets.length;i++){
 //     document.getElementById('set_select').innerHTML+=`<option value="${sets[i]}"> ${sets[i]} (${scores[sets[i]]})</option>`;
 // }
-// document.getElementById('set_select').innerHTML+=`<option >new</option>`;
-
-
-
-
-
-
-
-
-
-
+// document.getElementById('set_select').innerHTML+=`<option >new</option>`
